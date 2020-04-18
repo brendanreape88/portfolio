659c685639e8d3d7533f939891286dd2fcf5db64
Brendan Reape <brendanreape88@gmail.com>
2020-04-17 17:05:43 -0700

    added mobile nav menu

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $(".bio-link").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+$('.hamburger').on('click', function(event){
+  //  $('.mobile-menu-div').toggle(2000);
+    console.log('CLICKED')
   });
+  
+$('.bio-link').on('click', function(event){
+  document.querySelector('.bio').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+})
+
+$('.portfolio-link').on('click', function(event){
+  document.querySelector('.portfolio').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+})
