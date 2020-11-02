b268a6f3a9d3ad658e35dbc79ba5467216a725b0
Brendan Reape <brendanreape88@gmail.com>
2020-11-02 11:17:37 -0800

    added contact and improved CSS

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-$('.hamburger-div').on('click', function(event){
-  $('.mobile-menu-div').toggle(200);
-    console.log('CLICKED')
-  });
-  
-$('.bio-link').on('click', function(event){
-  document.querySelector('.bio').scrollIntoView({ 
-    behavior: 'smooth' 
-  });
-})
+$(".hamburger-div").on("click", function (event) {
+  $(".mobile-menu-div").toggle(200);
+  console.log("CLICKED");
+});
 
-$('.portfolio-link').on('click', function(event){
-  document.querySelector('.portfolio').scrollIntoView({ 
-    behavior: 'smooth' 
+$(".bio-link").on("click", function (event) {
+  document.querySelector(".bio").scrollIntoView({
+    behavior: "smooth",
   });
-})
+});
+
+$(".portfolio-link").on("click", function (event) {
+  document.querySelector(".portfolio").scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+$(".contact-link").on("click", function (event) {
+  document.querySelector(".contact").scrollIntoView({
+    behavior: "smooth",
+  });
+});
